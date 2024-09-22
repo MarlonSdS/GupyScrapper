66bdd71d6a91ff4189622036d718bea70a8af21e
Marlon dos Santos <marlondossantos03@outlook.com>
2024-09-22 20:32:57 -0300

    Pesquisa sendo feita. Versão 1.0 finalizada

--- main.py
+++ main.py
@@ -1,8 +1,91 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By 
+from time import sleep
+import json
+from datetime import date
+class GupyScrapper():
+    driver = webdriver.Firefox()
+    num_pages = 1
+    lista_vagas = {
+        'vaga': "",
+        'local': ""
+    }
+    def __init__(self):
+        #o site a ser raspado
+        url = "https://brisanet.gupy.io"
+        self.driver.get(url)
+        sleep(1)
+        self.reject_cookies()
+        self.scroll_down()
+        self.num_pages = self.get_num_pages()
+        self.driver.refresh()
+        sleep(1)
+        self.scroll_down()
+        self.get_jobs(self.num_pages)
+        self.save_json(self.lista_vagas)
+        self.check_interests(self.lista_vagas)
+        self.driver.quit()
 
-driver = webdriver.Firefox()
+    def reject_cookies(self):
+        """Retira o pop-up perguntando sobre o uso de cookies"""
+        sleep(1)
+        botao_cookie = self.driver.find_element(By.CLASS_NAME, 'dp-bar-button')
+        botao_cookie.click()
 
-url = "https://brisanet.gupy.io"
-driver.get(url)
+    def scroll_down(self):
+        """Scrola pra baixo para que o botão next fique visível"""
+        self.driver.execute_script('window.scrollBy(0,-100)')
 
-driver.quit()
+    def get_num_pages(self):
+        """Passa por várias páginas para poder verificar qual a última"""
+        print("\033[32m Verificando quantas páginas existem \033[m")
+        for i in range(15):
+            botao_proximo = self.driver.find_element(By.XPATH, '/html/body/div[1]/div/main/section[3]/div[3]/nav/ul/li[5]/button')
+            botao_proximo.click()
+        botao_ultimo = self.driver.find_element(By.XPATH, '/html/body/div[1]/div/main/section[3]/div[3]/nav/ul/li[4]/button')
+        num_pages = int(botao_ultimo.text)
+        print("\033[32m Há "+ str(num_pages) + " páginas \033[m")
+        return num_pages
+
+    def get_jobs(self, num_pages):
+        jobs_list = []
+        places_list = []
+        print("\033[32m Copiando cada vaga \033[m")
+        for page in range(num_pages):
+            vagas = self.driver.find_elements(By.CLASS_NAME, 'sc-f5007364-4')
+            locais = self.driver.find_elements(By.CLASS_NAME, 'KCtRP')
+            for vaga in vagas:
+                jobs_list.append(vaga.text)
+            for local in locais:
+                places_list.append(local.text)
+            #sleep(1)
+            botao_proximo = self.driver.find_element(By.XPATH, '/html/body/div[1]/div/main/section[3]/div[3]/nav/ul/li[5]/button')
+            botao_proximo.click()
+        print("\033[32m Foram encontradas "+ str(len(jobs_list)) +" vagas \033[m")
+        self.lista_vagas['vaga'] = jobs_list
+        self.lista_vagas['local'] = places_list
+
+    def save_json(self, lista_vagas):
+        json_data = json.dumps(lista_vagas)
+        data = date.today()
+        print("\033[32m Salvando dados em arquivo JSON \033[m")
+        with open(f"vagas {data}.json", "w") as arquivo:
+            arquivo.write(json_data)
+    
+    def check_interests(self, lista_vagas):
+        vagas = lista_vagas['vaga']
+        vagas_juazeiro = []
+        vagas_dev = []
+        for vaga in vagas:
+            if 'Juazeiro do Norte' in vaga:
+                vagas_juazeiro.append(vaga)
+            if 'Desenvolvedor' in vaga or'Programador' in vaga or 'Programação' in vaga or 'Back-End' in vaga or 'Front-End' in vaga:
+                vagas_dev.append(vaga)
+        print(f"\033[32m Foram encontradas {len(vagas_juazeiro)} vagas em Juazeiro do norte: \033[m")
+        for vaga in vagas_juazeiro:
+            print(f"\033[33m {vaga} \033[m")
+        print(f"\033[32m Foram encontradas {len(vagas_dev)} vagas na área dev: \033[m")
+        for vaga in vagas_dev:
+            print(f"\033[33m {vaga} \033[m")
+
+GupyScrapper()
--- readme.md
+++ readme.md
@@ -1,3 +1,21 @@
 # Gupy Scrapper
 Um scrapper feito com selenium que serve para entrar na página da gupy de algumas
 empresas e verificar se há vagas do meu interesse.
+
+## Para quê isso serve?
+Eu costumo de tempos em tempos olhar a página gupy de uma certa empresa para ver se tem vagas 
+novas que sejam do meu interesse porém a gupy tá longe de ser um bom portal para os candidatos, 
+há poucas opções de filtragem e não é possível ordenar as vagas da mais recente para a mais antiga,
+de modo que para saber se há vagas novas é preciso olhar todas e tentar lembrar quais vagas já estavam alí 
+e quais não. Por causa disso criei esse scrapper para me mostrar de forma rápida se há ou não vagas do meu interesse.
+
+## Como isso funciona?
+Utilizando a biblioteca selenium, uma navegação automatizada é feita na página da empresa no portal gupy.
+Primeiro, em uma aba do firefox, o scrapper clica no botão 'next' 15 vezes e então observa o número da última página.
+É assim que o programa sabe quantas páginas há ao todo (infelizmente a gupy não nos dá um jeito fácil de saber quantas 
+páginas existem ao todo, estamos contando com a sorte de que não existam mais do que 15).
+
+Então a página é recarregada e o programa passa a copiar os nomes das vagas da página e a passar para a próxima até 
+as páginas acabarem. Um arquivo json com as informações e a data do dia é salvo para consultas futuras e possíveis novas
+features que penso em adicionar no futuro. Por fim é feita uma verificação baseada em palavras chave para verificar quais vagas
+podem ser do meu interesse e estas são apresentadas no terminal.
